11d9c6a25df24020fb622d612eb541a1ec7148a5
unknown <thomas.vassallo@gmail.com>
2021-07-04 10:26:43 +0100

    added logic for the days.hours, mins, secs

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const countdown = () => {
-    const countDate = new Date('May 23, 2023 00:00:00');
-    console.log(countDate);
+    const countDate = new Date('May 23, 2023 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // calculate
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % day) / minute);
+    const textSecond = Math.floor((gap % day) / second);
+    console.log(textDay);
+    console.log(textHour);
+    console.log(textMinute);
+    console.log(textSecond);
+
+
 }
 
 countdown();
